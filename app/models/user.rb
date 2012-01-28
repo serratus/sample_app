f12dfbbc63ed1f4b0091a3a68fd04694f6431d68
Christoph Oberhofer <ch.oberhofer@gmail.com>
2012-01-28 11:07:57 +0100

    secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,10 @@
 #  updated_at :datetime        not null
 #
 
-
+require 'digest'
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
   
@@ -21,5 +22,34 @@ class User < ActiveRecord::Base
                     format: {with: email_regex},
                     uniqueness: {case_sensitive: false}
                     
+  validates :password, presence: true,
+                       length: {within: 6..40},
+                       confirmation: true
+         
+         
+  before_save :encrypt_password     
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+           
+  private
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+                    
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
 
