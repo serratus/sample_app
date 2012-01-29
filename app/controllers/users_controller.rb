9700c130e80c8d7f4f5b9ac53f34951f1432e01b
Christoph Oberhofer <ch.oberhofer@gmail.com>
2012-01-29 11:55:48 +0100

    User signup complete but tests do not work properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
-      render 'new'
+      render :new
     end
   end
 end
